077675c641412cb0a92f261a2011890d385e01fd
SimonMcGovern <simon.mc1070@gmail.com>
2024-04-11 11:21:10 -0300

    Se crea la funcion most_influent_player

--- hockey_stats/__init__.py
+++ hockey_stats/__init__.py
@@ -7,3 +7,13 @@ def team_max_striker(player_stats):
             max_striker_goals = player["goals"]
             max_striker = player["name"]
     return max_striker_goals, max_striker
+
+def most_influent_player(player_stats):
+    mvp_points = 0
+
+    for player in player_stats:
+        total_influent = player["goals"]*1.5 + player["goals_avoided"]*1.25 + player["assists"]
+        if total_influent > mvp_points:
+            mvp_points = total_influent
+            mvp = player["name"]
+    return mvp_points, mvp
