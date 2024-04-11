2e42c8de16e19f307bd970214844dd0d3e2bf8d8
SimonMcGovern <simon.mc1070@gmail.com>
2024-04-11 11:39:58 -0300

    Se agregaron los docstrings a las funciones del modulo hockey_stats

--- hockey_stats/__init__.py
+++ hockey_stats/__init__.py
@@ -1,5 +1,8 @@
 
 def team_max_striker(player_stats):
+    """
+    Esta funcion recibe una lista de diccionarios con las estadisticas de los jugadores y devuelve al maximx goleadorx del equipo y la cantidad de goles que hizo
+    """
     max_striker_goals = 0
 
     for player in player_stats:
@@ -9,6 +12,9 @@ def team_max_striker(player_stats):
     return max_striker_goals, max_striker
 
 def most_influent_player(player_stats):
+    """
+    Esta funcion recibe una lista de diccionarios con las estadisticas de los jugadores y devuelve al jugadorx más influyente del equipo y los puntos de influencia que consiguió
+    """
     mvp_points = 0
 
     for player in player_stats:
@@ -19,5 +25,8 @@ def most_influent_player(player_stats):
     return mvp_points, mvp
 
 def team_goals(player_stats):
+    """
+    Esta funcion recibe una lista de diccionarios con las estadisticas de los jugadores y devuelve la cantidad de goles totales que metió el equipo
+    """
     total_goals = sum(map(lambda player: player["goals"], player_stats))
     return total_goals
